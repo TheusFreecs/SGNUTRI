29200567eb6d8f957c1074a6991e039bf95f473d
TheusFreecs <matheus150995@gmail.com>
2021-09-10 14:12:45 -0300

    first commit

---
+++ SistemaGerenciamentoNutricional/SGNUTRI/Frm_Login.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace SGNUTRI
+{
+    partial class Frm_Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Login));
+            this.Btn_Sair = new System.Windows.Forms.Button();
+            this.Txt_Usuario = new System.Windows.Forms.TextBox();
+            this.Txt_Senha = new System.Windows.Forms.TextBox();
+            this.Btn_Entrar = new System.Windows.Forms.Button();
+            this.Lbl_Usuario = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Sair
+            // 
+            this.Btn_Sair.Location = new System.Drawing.Point(689, 428);
+            this.Btn_Sair.Name = "Btn_Sair";
+            this.Btn_Sair.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Sair.TabIndex = 0;
+            this.Btn_Sair.Text = "Sair";
+            this.Btn_Sair.UseVisualStyleBackColor = true;
+            this.Btn_Sair.Click += new System.EventHandler(this.Btn_Sair_Click);
+            // 
+            // Txt_Usuario
+            // 
+            this.Txt_Usuario.BackColor = System.Drawing.Color.ForestGreen;
+            this.Txt_Usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Txt_Usuario.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Txt_Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Usuario.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Txt_Usuario.Location = new System.Drawing.Point(282, 119);
+            this.Txt_Usuario.Multiline = true;
+            this.Txt_Usuario.Name = "Txt_Usuario";
+            this.Txt_Usuario.Size = new System.Drawing.Size(364, 38);
+            this.Txt_Usuario.TabIndex = 1;
+            this.Txt_Usuario.Text = "Usuário";
+            this.Txt_Usuario.Enter += new System.EventHandler(this.Txt_Usuario_Enter);
+            this.Txt_Usuario.Leave += new System.EventHandler(this.Txt_Usuario_Leave);
+            // 
+            // Txt_Senha
+            // 
+            this.Txt_Senha.BackColor = System.Drawing.Color.ForestGreen;
+            this.Txt_Senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Txt_Senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.Txt_Senha.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Txt_Senha.Location = new System.Drawing.Point(282, 186);
+            this.Txt_Senha.Multiline = true;
+            this.Txt_Senha.Name = "Txt_Senha";
+            this.Txt_Senha.Size = new System.Drawing.Size(364, 36);
+            this.Txt_Senha.TabIndex = 1;
+            this.Txt_Senha.Text = "Senha";
+            this.Txt_Senha.TextChanged += new System.EventHandler(this.Txt_Senha_TextChanged);
+            this.Txt_Senha.Enter += new System.EventHandler(this.Txt_Senha_Enter);
+            this.Txt_Senha.Leave += new System.EventHandler(this.Txt_Senha_Leave);
+            // 
+            // Btn_Entrar
+            // 
+            this.Btn_Entrar.BackColor = System.Drawing.Color.ForestGreen;
+            this.Btn_Entrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_Entrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.Btn_Entrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.Btn_Entrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(118)))), ((int)(((byte)(126)))));
+            this.Btn_Entrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Entrar.ForeColor = System.Drawing.Color.Black;
+            this.Btn_Entrar.Location = new System.Drawing.Point(295, 243);
+            this.Btn_Entrar.Name = "Btn_Entrar";
+            this.Btn_Entrar.Size = new System.Drawing.Size(329, 62);
+            this.Btn_Entrar.TabIndex = 4;
+            this.Btn_Entrar.Text = "Entrar";
+            this.Btn_Entrar.UseVisualStyleBackColor = false;
+            this.Btn_Entrar.Click += new System.EventHandler(this.Btn_Entrar_Click);
+            // 
+            // Lbl_Usuario
+            // 
+            this.Lbl_Usuario.AutoSize = true;
+            this.Lbl_Usuario.BackColor = System.Drawing.Color.ForestGreen;
+            this.Lbl_Usuario.Font = new System.Drawing.Font("Century Gothic", 35.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Usuario.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Lbl_Usuario.Location = new System.Drawing.Point(371, 18);
+            this.Lbl_Usuario.Name = "Lbl_Usuario";
+            this.Lbl_Usuario.Size = new System.Drawing.Size(175, 57);
+            this.Lbl_Usuario.TabIndex = 6;
+            this.Lbl_Usuario.Text = "LOGIN";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(279, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "────\t\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t──\t─\t" +
+    "─\t─\t─\t\t";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(367, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t─\t──\t" +
+    "─\t─\t─\t─\t\t";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SGNUTRI.Properties.Resources.minimazar;
+            this.pictureBox2.Location = new System.Drawing.Point(590, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(34, 29);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_2);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::SGNUTRI.Properties.Resources.cerrar;
+            this.pictureBox3.Location = new System.Drawing.Point(627, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 29);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 17;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SGNUTRI.Properties.Resources.Food_Health_System;
+            this.pictureBox4.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(259, 332);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox4_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1221, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(658, 330);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Lbl_Usuario);
+            this.Controls.Add(this.Btn_Entrar);
+            this.Controls.Add(this.Txt_Senha);
+            this.Controls.Add(this.Txt_Usuario);
+            this.Controls.Add(this.Btn_Sair);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Frm_Login";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SGNUTRI - Sistema Gerenciamento Nutricional";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Frm_Login_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Sair;
+        private System.Windows.Forms.TextBox Txt_Usuario;
+        private System.Windows.Forms.TextBox Txt_Senha;
+        private System.Windows.Forms.Button Btn_Entrar;
+        private System.Windows.Forms.Label Lbl_Usuario;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ SistemaGerenciamentoNutricional/SGNUTRI/Frm_Login.cs
@@ -0,0 +1,157 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SGNUTRI {
+    public partial class Frm_Login : Form {
+        public Frm_Login() {
+            InitializeComponent();
+            Txt_Senha.PasswordChar = '*';
+
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+
+
+        private void Btn_Sair_Click(object sender, EventArgs e) {
+            if (MessageBox.Show("Deseja realmente Sair?", "Mensagem Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) ;
+            {
+                Application.Exit();
+            }
+
+
+        }
+
+        private void Txt_Senha_TextChanged(object sender, EventArgs e) {
+            if (Txt_Senha.Text == "Senha") {
+                Txt_Senha.Text = "";
+                Txt_Senha.ForeColor = Color.Black;
+
+            }
+        }
+
+        
+
+        private void Btn_Entrar_Click(object sender, EventArgs e) {
+            MySqlConnection conexaoBD = new MySqlConnection(Conect.strConect);
+            conexaoBD.Open();
+            MySqlCommand cmd = new MySqlCommand("select * from tbfuncionario where NOME= '" + Txt_Usuario.Text + "' and CARGO='" + Txt_Senha.Text + "'", conexaoBD);
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read()) {
+                if (Txt_Usuario.Text == reader.GetString("NOME")) {
+
+
+                    if (Txt_Senha.Text == reader.GetString("CARGO")) {
+                        MessageBox.Show("Login realizado com sucesso");
+                        Frm_Menu menu = new Frm_Menu();
+                        menu.Show();
+                    }
+                    else {
+                        MessageBox.Show("Login invalido");
+                    }
+                }
+            }
+
+            conexaoBD.Close();
+        }
+
+        
+
+        
+
+        
+
+        
+
+
+
+        
+
+        
+
+        
+        private void pictureBox3_Click(object sender, EventArgs e) {
+            if (MessageBox.Show("Deseja realmente Sair?", "Mensagem Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) ;
+            {
+                Application.Exit();
+            }
+        }
+
+        
+
+        private void pictureBox2_Click_1(object sender, EventArgs e) {
+            if (MessageBox.Show("Deseja realmente Sair?", "Mensagem Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) ;
+            {
+                Application.Exit();
+            }
+
+        }
+
+        private void Txt_Usuario_Enter(object sender, EventArgs e) {
+            if (Txt_Usuario.Text == "Usuário") {
+                Txt_Usuario.Text = "";
+                Txt_Usuario.ForeColor = Color.Black;
+
+            }
+        }
+
+        private void Txt_Usuario_Leave(object sender, EventArgs e) {
+            if (Txt_Usuario.Text == "") {
+                Txt_Usuario.Text = "Usuário";
+                Txt_Usuario.ForeColor = Color.Black;
+            }
+        }
+
+        
+
+        
+
+        private void Txt_Senha_Leave(object sender, EventArgs e) {
+            if (Txt_Senha.Text == "") {
+                Txt_Senha.Text = "Senha";
+                Txt_Senha.ForeColor = Color.Black;
+            }
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e) {
+            if (MessageBox.Show("Deseja realmente Sair?", "Mensagem Alerta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) ;
+            {
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox2_Click_2(object sender, EventArgs e) {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Frm_Login_MouseDown(object sender, MouseEventArgs e) {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void pictureBox4_MouseDown(object sender, MouseEventArgs e) {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+
+        }
+
+        private void Txt_Senha_Enter(object sender, EventArgs e) {
+            if (Txt_Senha.Text == "Senha") {
+                Txt_Senha.Text = "";
+                Txt_Senha.ForeColor = Color.Black;
+            }
+        }
+    }
+}
+
+
+
+
